43c692504509353e47c023d28fe885685026ada1
Marija Antic <m.antic@levi9.com>
2019-11-08 17:16:14 +0100

    setup tests coverage and react testing library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Test Layout component', () => {
+  it('should render correctly', () => {
+    const { container } = render(<App />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
